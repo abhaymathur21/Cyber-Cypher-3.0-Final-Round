bea900628d592bb26db23d6e0d0ab8068fd847dd
Abhay Mathur <a21.mathur21@gmail.com>
2024-02-08 22:18:45 +0530

    key word extraction

---
+++ gemini.py
@@ -0,0 +1,49 @@
+"""
+At the command line, only need to run once to install the package via pip:
+
+$ pip install google-generativeai
+"""
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyAICt3N6_7M5k1rabLvPESlCixWHyzsq6o")
+
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+input_text = input("Enter the text: ")
+
+prompt_parts = [
+  f"Extract the item to be bought and its quantity from the text that will be provided and then give a string output with only the item separated from just the numeric value of the quantity using a comma. The output string must not contain anything but the item name and quantity value. The text to be used is: {input_text}"
+]
+
+response = model.generate_content(prompt_parts)
+print(response.text)
