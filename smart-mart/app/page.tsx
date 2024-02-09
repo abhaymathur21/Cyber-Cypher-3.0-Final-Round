2e6ecdac473e86293b0c7022f98d246f4cecd9c0
Devansh Rathor <devanshrathor123@gmail.com>
2024-02-09 13:24:59 +0530

    chat bot fix

--- smart-mart/app/page.tsx
+++ smart-mart/app/page.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "@/components/ProductCard";
 import products from "@/data/products.json";
 import { searchProducts } from "@/lib/utils";
+import { log } from "console";
 import Link from "next/link";
 
 export default async function Home({
@@ -12,10 +13,13 @@ export default async function Home({
     ? searchProducts(products, q)
     : id
       ? products.filter((product) =>
-          id.split(",").map(parseInt).includes(product.id),
+          id
+            .split(",")
+            .map((x) => parseInt(x.trim()))
+            .includes(product.id),
         )
       : products;
-
+  if (id) log(data, id.split(","));
   return (
     <div className="grid max-h-full grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-8 overflow-y-auto pr-4 [grid-auto-rows:1fr;]">
       {data.length > 0 ? (
@@ -23,7 +27,7 @@ export default async function Home({
           <ProductCard key={product.id} product={product} />
         ))
       ) : (
-        <div className="grid h-min gap-2">
+        <div className="col-span-4 col-start-1 grid h-min gap-2">
           <div className="mt-8 text-center text-2xl">No products found</div>
           <Link href="/" className="mt-4 text-center text-xl text-secondary">
             Clear search
