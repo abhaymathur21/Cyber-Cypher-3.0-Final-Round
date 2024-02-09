3e94e678a061246bca515935ccf0e88395c17345
Devansh Rathor <devanshrathor123@gmail.com>
2024-02-09 16:46:25 +0530

    link fix

--- smart-mart/components/CartCard.tsx
+++ smart-mart/components/CartCard.tsx
@@ -21,32 +21,32 @@ const CartCard = ({
   if (!product) return null;
 
   return (
-    <Link href={`/products/${product.id}`}>
-      <Card className="grid h-min">
+    <Card className="grid h-min">
+      <Link href={`/products/${product.id}`}>
         <CardHeader>
           <CardTitle>{product.name}</CardTitle>
         </CardHeader>
-        <CardContent>
-          <CardDescription className="grid max-w-fit grid-cols-[auto_1fr] gap-4">
-            <Image
-              src={`https://picsum.photos/seed/${product.id}/300/300`}
-              alt={product.name}
-              width={300}
-              height={300}
-              className="aspect-video size-16 place-self-center rounded-md border-2 border-gray-950 object-cover object-center"
-            />
-            <div>
-              <p className="max-w-[40ch] text-pretty">{product.description}</p>
-              <p>${product.price}</p>
-            </div>
-          </CardDescription>
-        </CardContent>
-        <CardFooter className="flex items-center justify-between">
-          <CartAmount id={id} quantity={quantity} />
-          <CartRemove id={id} />
-        </CardFooter>
-      </Card>
-    </Link>
+      </Link>
+      <CardContent>
+        <CardDescription className="grid max-w-fit grid-cols-[auto_1fr] gap-4">
+          <Image
+            src={`https://picsum.photos/seed/${product.id}/300/300`}
+            alt={product.name}
+            width={300}
+            height={300}
+            className="aspect-video size-16 place-self-center rounded-md border-2 border-gray-950 object-cover object-center"
+          />
+          <div>
+            <p className="max-w-[40ch] text-pretty">{product.description}</p>
+            <p>${product.price}</p>
+          </div>
+        </CardDescription>
+      </CardContent>
+      <CardFooter className="flex items-center justify-between">
+        <CartAmount id={id} quantity={quantity} />
+        <CartRemove id={id} />
+      </CardFooter>
+    </Card>
   );
 };
 
